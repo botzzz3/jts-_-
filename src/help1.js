5a8b4e52f125da2e63d77b9b1a7b1d9a3edd1225
botzzz3 <77111954+botzzz3@users.noreply.github.com>
2021-03-30 00:31:46 -0300

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,118 @@
+const help1 = (prefix, sender) => {
+	return `LOJA JTS :)
+
+SEJA BEM VINDO(A) AO GRUPO JXL STORE
+====================================
+      [COMANDOS EM BAIXO]
+ TODOS OS COMANDOS DIGITE  COM (!)
+
+ [!]STICKER( SO USE SE O DONO DEIXAR
+ [!]REGRAS
+ [!]DONO
+COMANDOS DIVERSOS OPS: SO SE O ADM AUTORIZAR
+࿇ ══━━━━✥◈✥━━━━══ ࿇
+    Outros Menus
+    ❏${prefix}nsfwmenu
+    ❏${prefix}menuadmin
+  
+    
+  ◪ *LEVEL*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}leveling
+    └─ ❏ ${prefix}level
+  ◪ *CRIAR*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}sticker
+    ├─ ❏ ${prefix}toimg
+    ├─ ❏ ${prefix}bpink
+    ├─ ❏ ${prefix}marvellogo
+    ├─ ❏ ${prefix}snowwrite
+    ├─ ❏ ${prefix}3dtext
+    ├─ ❏ ${prefix}ninjalogo
+    ├─ ❏ ${prefix}water
+    ├─ ❏ ${prefix}firetext
+    ├─ ❏ ${prefix}neonlogo
+    ├─ ❏ ${prefix}lionlogo
+    ├─ ❏ ${prefix}jokerlogo
+    └─ ❏ ${prefix}quotemaker
+  ◪ *PESQUISA*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}wiki
+    ├─ ❏ ${prefix}wikien
+    ├─ ❏ ${prefix}nulis
+    └─ ❏ ${prefix}artinama
+  ◪ *DOWNLOADER*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}images 
+    ├─ ❏ ${prefix}ytmp3
+    ├─ ❏ ${prefix}ytmp4
+    ├─ ❏ ${prefix}joox
+    ├─ ❏ ${prefix}joox2
+    ├─ ❏ ${prefix}trendtwit
+    └─ ❏ ${prefix}ytsearch
+  ◪ *MEME*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}meme
+    └─ ❏ ${prefix}memeindo
+  ◪ *SOM*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}play
+    └─ ❏ ${prefix}tts
+  ◪ *MUSICA*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}lirik
+    └─ ❏ ${prefix}chord
+  ◪ *STALKEAR*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}tiktokstalk
+    └─ ❏ ${prefix}igstalk
+  ◪ *WIBU*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}neonime
+    ├─ ❏ ${prefix}pokemon
+    ├─ ❏ ${prefix}loli
+    ├─ ❏ ${prefix}waifu
+    ├─ ❏ ${prefix}wallpaper
+    ├─ ❏ ${prefix}neko
+    └─ ❏ ${prefix}nekonime
+  ◪ *DIVERSÃO*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}alay
+    ├─ ❏ ${prefix}gantengcek
+    ├─ ❏ ${prefix}watak
+    ├─ ❏ ${prefix}hobby
+    ├─ ❏ ${prefix}game
+    ├─ ❏ ${prefix}simih
+    └─ ❏ ${prefix}glitch
+  ◪ *INFORMAÇÂO*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}bahasa
+    ├─ ❏ ${prefix}kodenegara
+    ├─ ❏ ${prefix}kbbi
+    ├─ ❏ ${prefix}fakta
+    ├─ ❏ ${prefix}infocuaca
+    └─ ❏ ${prefix}covid
+  ◪ *OUTROS*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}send
+    ├─ ❏ ${prefix}wame
+    ├─ ❏ ${prefix}virtex
+    ├─ ❏ ${prefix}exe
+    ├─ ❏ ${prefix}qrcode
+    ├─ ❏ ${prefix}quotes
+    └─ ❏ ${prefix}fml.  `
+}
+
+exports.help1 = help1
---
+++ src/regras.js
@@ -0,0 +1,19 @@
+const regras = (prefix, sender) => {
+	return `regras
+
+⚠️Caso for pedir jogos escreva! Não mande áudio (não prometemos atender todos os pedidos,mas vamos tentar o máximo possível).⚠️
+
+💥 Regras 💥
+
+Proibido qualquer tipo de link
+
+Se for para brigar briga no PV
+
+Mandou o jogo espere não mande duas vezes
+
+Não chamar ADM no privado se quiser falar alguma coisa fale no grupo
+
+ADM tem vida também então não enche o saco para adicionar jogo o dia inteiro`
+}
+
+exports.regras = regras
